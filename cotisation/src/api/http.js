f8fc608f287d68fbb7b3a078c6b9c788bc4cd39e
abdirahman <abdirahmanneymar3533@gmail.com>
2026-06-01 09:19:40 +0300

    je fixe l'api url

--- cotisation/src/api/http.js
+++ cotisation/src/api/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
+  baseURL: import.meta.env.VITE_API_URL || "https://hiil-foundation-api.onrender.com",
 });
 
 http.interceptors.request.use((config) => {
